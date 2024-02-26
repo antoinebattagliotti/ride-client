8f871d17b284fa6e942e4e8e7f734dde5034b6fb
Antoine BATTAGLIOTTI <antoinebattagliotti@outlook.com>
2024-02-26 21:35:09 +0100

    updgrade dependencie async storage

--- App.tsx
+++ App.tsx
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ color: '#3358D4' }}>
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F9F9FB',
     alignItems: 'center',
     justifyContent: 'center',
   },
